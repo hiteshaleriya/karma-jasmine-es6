8136234697af5ec1c743d5aee92476afdef12618
Hitesh Aleriya <hiteshaleriya@gmail.com>
2017-04-21 23:57:47 +0530

    added travis config

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,5 +9,5 @@ module.exports = function(grunt) {
     });
     
     grunt.file.expand('./node_modules/grunt-*/tasks').forEach(grunt.loadTasks);
-    grunt.registerTask('test', ['karma']);
+    grunt.registerTask('default', ['karma']);
 };
